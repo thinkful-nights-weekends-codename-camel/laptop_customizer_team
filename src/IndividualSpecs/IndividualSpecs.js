3d665ed04ec9841565a1e42a6629e05098710b62
Jeanine Hoffman <jeanine.hoffman1970@gmail.com>
2019-05-22 16:41:29 -0700

    still broken but less errors

--- src/IndividualSpecs/IndividualSpecs.js
+++ src/IndividualSpecs/IndividualSpecs.js
@@ -5,22 +5,22 @@ export default class IndividualSpecs extends Component {
 
   render() {
 
-    const features = Object.keys(this.props.features).map(key => {
+    const features = Object.keys(this.props.features)
+    .map(key => {
       const options = this.props.features[key].map((item, index) => {
-        const selectedClass = item.name === this.props.selected[key].name ? 'feature__selected' : '';
+        const selectedClass = item.name === this.state.selected[key].name ? 'feature__selected' : '';
         const featureClass = 'feature__option ' + selectedClass;
-
         return <li key={index} className="feature__item">
-          <div className={featureClass}>
-
+          <div className={featureClass}
+            
             onClick={e => this.updateFeature(key, item)}>
-             {item.name}
-            ({new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' })
-              .format(item.cost)})
+              { item.name }
+              ({ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
+                .format(item.cost) })
           </div>
         </li>
-
       });
+      
       return <div className="feature" key={key}>
         <div className="feature__name">{key}</div>
         <ul className="feature__list">
